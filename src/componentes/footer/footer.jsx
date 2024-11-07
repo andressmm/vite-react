159693c4ceb6b6c722e2f1eed78cffde0c9adc07
Andres <161887016+andressmm@users.noreply.github.com>
2024-11-07 03:10:47 +0100

    Update footer.jsx

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -2,6 +2,7 @@ import './estilo.css';
 function Footer(){
     return(
         <p className='footer'>
+            <hr>
         <h2>Tienda de electronica</h2>
             <h4>Alumno: Andres Moran</h4>
         </p>
